40e1b210021bc29445affc79dedbab828472df1f
ginkgoch <ginkgoch@outlook.com>
2018-07-28 19:11:04 +0800

    add index

---
+++ index.js
@@ -0,0 +1,7 @@
+const Shapefile = require('./libs/Shapefile');
+const ShapefileIterator = require('./libs/ShapefileIterator');
+const ShapefileType = require('./libs/ShapefileType');
+
+module.exports = {
+    Shapefile, ShapefileIterator, ShapefileType
+};
