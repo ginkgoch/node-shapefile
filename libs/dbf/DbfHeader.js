1c1512a09cdc205a0a9669602ba545a9f8418d2b
ginkgoch <ginkgoch@outlook.com>
2019-07-13 18:01:24 +0800

    Implement header write method.

--- libs/dbf/DbfHeader.js
+++ libs/dbf/DbfHeader.js
@@ -1,5 +1,5 @@
 const fs = require('fs')
-const { BufferReader } = require('ginkgoch-buffer-io')
+const { BufferReader, BufferWriter } = require('ginkgoch-buffer-io')
 
 module.exports = class DbfHeader {
     constructor() {
@@ -30,21 +30,65 @@ module.exports = class DbfHeader {
         this.fields = [];
         let position = headerBuffer.length;
         while(position < this.headerLength - 1) { 
-            const columnBuffer = Buffer.alloc(32);
-            fs.readSync(fileDescriptor, columnBuffer, 0, columnBuffer.length, position);
+            const fieldBuffer = Buffer.alloc(32);
+            fs.readSync(fileDescriptor, fieldBuffer, 0, fieldBuffer.length, position);
 
             const field = { };
-            field.name = columnBuffer.slice(0, 11).toString().replace(/\0/g, '').trim();
-            field.type = String.fromCharCode(columnBuffer.readUInt8(11));
+            field.name = fieldBuffer.slice(0, 11).toString().replace(/\0/g, '').trim();
+            field.type = String.fromCharCode(fieldBuffer.readUInt8(11));
             if(field.type.toUpperCase() === 'C') {
-                field.length = columnBuffer.readUInt16LE(16);
+                field.length = fieldBuffer.readUInt16LE(16);
             } else {
-                field.length = columnBuffer.readUInt8(16);
-                field.decimal = columnBuffer.readUInt8(17);
+                field.length = fieldBuffer.readUInt8(16);
+                field.decimal = fieldBuffer.readUInt8(17);
             }
 
             this.fields.push(field);
-            position += columnBuffer.length;
+            position += fieldBuffer.length;
         }
+    }
+
+    write(fileDescriptor) {
+        const headerBuffer = Buffer.alloc(32);
+        const headerWriter = new BufferWriter(headerBuffer)
+        headerWriter.writeInt8(this.fileType)
+        headerWriter.writeInt8(this.year)
+        headerWriter.writeInt8(this.month)
+        headerWriter.writeInt8(this.day)
+        headerWriter.writeUInt32(this.recordCount)
+        headerWriter.writeUInt16(this.headerLength)
+        headerWriter.writeUInt16(this.recordLength)
+        fs.writeSync(fileDescriptor, headerBuffer, 0, headerBuffer.length, 0)
+
+        let position = headerBuffer.length;
+        for (let field of this.fields ) {
+            let fieldBuffer = Buffer.alloc(32);
+            const fieldWriter = new BufferWriter(fieldBuffer)
+
+            const fieldNameBuffer = this._chunkFieldNameBuffer(field.name)
+            fieldWriter.writeBuffer(fieldNameBuffer)
+
+            const fieldTypeCode = field.type.charCodeAt(0)
+            fieldWriter.writeUInt8(fieldTypeCode)
+
+            fieldWriter.seek(16)
+            if (field.type.toUpperCase() === 'C') {
+                fieldWriter.writeUInt16(field.length)
+            } else {
+                fieldWriter.writeUInt8(field.length)
+                fieldWriter.writeUInt8(field.decimal)
+            }
+
+            fs.writeSync(fileDescriptor, fieldBuffer, 0, fieldBuffer.length, position)
+            position += fieldBuffer.length
+
+        }
+    }
+
+    _chunkFieldNameBuffer(fieldName) {
+        const fieldNameBuffer = Buffer.alloc(11)
+        const sourceBuffer = Buffer.from(fieldName)
+        sourceBuffer.copy(fieldNameBuffer, 0, 0, sourceBuffer.length > 11 ? 11 : sourceBuffer.length)
+        return fieldNameBuffer
     }
 }
--- tests/DbfHeader.test.js
+++ tests/DbfHeader.test.js
@@ -9,6 +9,67 @@ describe('DbfHeader tests', () => {
         const header = new DbfHeader()
         header.read(_fd)
 
-        console.log(header)
+        expect(header.fields.length).not.toBe(0)
+    })
+
+    test('test chunk field name buffer', () => {
+        const header = new DbfHeader()
+
+        // length equal to 10
+        let fieldName = 'HelloWorld'
+        let buffer1 = header._chunkFieldNameBuffer(fieldName)
+        expect(buffer1.length).toBe(11)
+
+        let fieldName1 = buffer1.toString()
+        expect(fieldName1).toBe(fieldName + '\0')
+
+        // length less than 10
+        fieldName = 'Hello'
+        buffer1 = header._chunkFieldNameBuffer(fieldName)
+        expect(buffer1.length).toBe(11)
+
+        fieldName1 = buffer1.toString().replace(/\0/g, '').trim()
+        expect(fieldName1).toBe(fieldName)
+
+
+        // length less than 10
+        fieldName = 'HelloWorldHelloWorld'
+        buffer1 = header._chunkFieldNameBuffer(fieldName)
+        expect(buffer1.length).toBe(11)
+
+        fieldName1 = buffer1.toString().replace(/\0/g, '').trim()
+        expect(fieldName1).toBe('HelloWorldH')
+    })
+
+    test('char and buffer conversion', () => {
+        let c = String.fromCharCode(66)
+        let cc = c.charCodeAt(0)
+        expect(cc).toBe(66)
+    })
+
+    test('write header', () => {
+        const header = new DbfHeader()
+        header.fileType = 3
+        header.year = 105
+        header.month = 2
+        header.day = 6
+        header.recordCount = 51
+        header.headerLength = 64
+        header.recordLength = 467
+        header.fields.push({ name: 'AREA', type: 'N', length: 12, decimal: 3 })
+
+        const filePath = './test_header_writer.dbf'
+        let _fd = fs.openSync(filePath, 'w+')
+        header.write(_fd)
+        fs.closeSync(_fd)
+
+        _fd = fs.openSync(filePath, 'rs')
+        const header1 = new DbfHeader()
+        header1.read(_fd)
+        fs.closeSync(_fd)
+
+        expect(header1).toStrictEqual(header)
+
+        fs.unlinkSync(filePath)
     })
 })
