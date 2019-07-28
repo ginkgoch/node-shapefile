eb37520808c5ea65707f31b999f272c5c2f23a92
ginkgoch <ginkgoch@outlook.com>
2019-07-28 23:43:14 +0800

    Remove some unnecessary methods.

--- src/shp/parser/GeomParser.ts
+++ src/shp/parser/GeomParser.ts
@@ -45,7 +45,7 @@ export default abstract class GeomParser {
     write(coordinates: any, writer: ShpWriter): void {
         writer.writeInt32LE(this.type);
         if (this.type !== ShapefileType.point) {
-            const envelope = Envelope.from(GeomParser.coordinates(coordinates));
+            const envelope = Envelope.from(coordinates);
             writer.writeEnvelope(envelope);
         }
 
@@ -71,22 +71,6 @@ export default abstract class GeomParser {
     }
 
     protected abstract _size(coordinates: any): number;
-
-    //TODO: remove this method.
-    static vertices(coordinates: any): number[][] {
-        const vertices = new Array<number[]>();
-        const flatten = _.flattenDeep(coordinates) as number[];
-        for (let i = 0; i < flatten.length; i += 2) {
-            vertices.push([flatten[i], flatten[i + 1]]);
-        }
-
-        return vertices;
-    }
-
-    //TODO: remove this method.
-    static coordinates(coordinates: any): ICoordinate[] {
-        return GeomParser.vertices(coordinates).map(c => ({ x: c[0], y: c[1] }));
-    }
 
     getGeomBuff(geom: Geometry): Buffer {
         const coordinates = geom.coordinates();
