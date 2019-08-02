fec8473c327ce5a74ff405aecdee0cd54ea701a7
Howardch <howardch@Howards-MacBook-Pro.local>
2019-08-03 07:48:11 +0800

    Add webpack template.

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,56 @@
 const path = require('path');
 module.exports = {
   mode: 'development',
-  entry: path.join(__dirname, 'src', 'index'),
+  entry: path.join(__dirname, 'src', 'index.ts'),
   watch: true,
+  target: 'node',
   output: {
     path: __dirname + 'dist',
-    publicPath: '/dist/',
     filename: "bundle.js",
-    chunkFilename: '[name].js'
+  },
+  node: {
+    fs: false
   },
   module: {
-    rules: [{
-      test: /.jsx?$/,
-      include: [
-        path.resolve(__dirname, 'src')
-      ],
-      exclude: [
-        path.resolve(__dirname, 'node_modules')
-      ],
-      loader: 'babel-loader',
-      query: {
-        presets: [
-          ["@babel/env", {
-            "targets": {
-              "browsers": "last 2 chrome versions"
-            }
-          }]
+    rules: [
+      {
+        loader: 'ts-loader',
+        test: /\.ts$/,
+        exclude: [
+          path.resolve(__dirname, 'built'),
+          path.resolve(__dirname, 'node_modules'),
+          path.resolve(__dirname, 'tests'),
         ]
       }
-    }]
+    ]
+  // {
+  //     test: /.jsx?$/,
+  //     include: [
+  //       path.resolve(__dirname, 'built')
+  //     ],
+  //     exclude: [
+  //       path.resolve(__dirname, 'node_modules')
+  //     ],
+  //     loader: 'babel-loader',
+  //     query: {
+  //       presets: [
+  //         ["@babel/env", {
+  //           "targets": {
+  //             "browsers": "last 2 chrome versions"
+  //           }
+  //         }]
+  //       ]
+  //     }
+  //   }]
   },
   resolve: {
-    extensions: ['.json', '.js', '.jsx']
+    extensions: ['.json', '.js', '.jsx', '.ts']
   },
   devtool: 'source-map',
-  devServer: {
-    contentBase: path.join('/dist/'),
-    inline: true,
-    host: '0.0.0.0',
-    port: 8080,
-  }
+  // devServer: {
+  //   contentBase: path.join('/dist/'),
+  //   inline: true,
+  //   host: '0.0.0.0',
+  //   port: 8080,
+  // }
 };
