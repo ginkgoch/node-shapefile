ce099ce7011b7441070ccdaf4480e235dd86b5a1
Ginkgoch <ginkgoch@outlook.com>
2020-02-18 12:05:42 +0800

    Upgrade geom dep for a disjoined issue.

--- tests/shp/Envelope.test.ts
+++ tests/shp/Envelope.test.ts
@@ -15,9 +15,9 @@ describe('envelope helper test', () => {
         env2 = { minx: -5, miny: 0, maxx: 5, maxy: 10 };
         expect(Envelope.disjoined(env1, env2)).toBeFalsy();
 
-        expect(Envelope.disjoined(env1, undefined)).toBeFalsy();
+        expect(Envelope.disjoined(env1, undefined)).toBeTruthy();
         
-        expect(Envelope.disjoined(undefined, undefined)).toBeFalsy();
+        expect(Envelope.disjoined(undefined, undefined)).toBeTruthy();
     });
 
     test('equals', () => {
